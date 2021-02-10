8fdef30e24f6065b8c14aa344947333007321c94
alex.whittle <alex.whittle@red-owl.co.uk>
2021-02-10 15:28:58 +0000

    s

--- Helpers/BasicAuthenticationHeaderValue.cs
+++ Helpers/BasicAuthenticationHeaderValue.cs
@@ -5,7 +5,6 @@ namespace LaunchPad.Client.Helpers
     internal class BasicAuthenticationHeaderValue : AuthenticationHeaderValue
     {
         public BasicAuthenticationHeaderValue(string scheme, string parameter) : base(scheme, parameter)
-        {
-        }
+        { }
     }
 }
